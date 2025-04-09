5de5d0f32f9024c164b0006fbe33808ff5e9e915
Tapiwa Ndemera <bibiralph57@gmail.com>
2025-04-09 15:30:59 +0200

    Project complete

---
+++ register_credentials.py
@@ -0,0 +1,10 @@
+import yagmail
+
+# Replace with your email and app password
+email ='ralphbibi57@gmail.com'
+app_password ='chzgncuasxfbypqp'
+
+# Register credentials with yagmail
+yagmail.register(email, app_password)
+
+print('Credentials registered successfully!')
