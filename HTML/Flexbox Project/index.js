b6ea6cfe7ea9cc22f2f46cca8be19fd95ab1de23
Tapiwa Ndemera <bibiralph57@gmail.com>
2025-06-04 13:48:47 +0200

    Html project

---
+++ HTML/Flexbox Project/index.js
@@ -0,0 +1,11 @@
+
+document.getElementById("text").textContent="How are you"; 
+document.getElementById("sty").innerHTML = "<h2>Tapiwa ndemera</h2>";
+const Picture= document.getElementById("Menu");
+Picture.src = ("./pics/iphone.png");
+Picture.alt = ("Iphone")
+document.getElementById("free").style="red"
+document.getElementById("out").addEventListener("click", function(){
+alert("House on fire")
+});
+
